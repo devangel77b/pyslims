a68f7647e745f6d77f98a221ca4e0436b05f4443
Dennis Evangelista <devangel77b@gmail.com>
2026-08-30 19:18:04 -0400

    tried outputing csv but SLiMS ignores the header! fuck that shit go back to inserting it into the mysql database

--- import_isbn.py
+++ import_isbn.py
@@ -2,6 +2,10 @@
 
 import argparse
 import logging
+import requests
+import csv
+import os
+import sys
 
 if __name__ == "__main__":
 
@@ -23,6 +27,19 @@ if __name__ == "__main__":
         logging.basicConfig(level=logging.WARNING)
 
     logging.debug(f"Working on ISBNs: {args.isbn}")
+
+    # setup output
+    headers_csv = ['title','isbn_issn','publisher_name','publish_year','authors']
+    ofile = sys.stdout
+    header_writer = csv.writer(
+        ofile,
+        quoting=csv.QUOTE_NONE)
+    header_writer.writerow(headers_csv)
+    
+    writer = csv.DictWriter(
+        ofile,
+        fieldnames=headers_csv,
+        quoting=csv.QUOTE_ALL)
     
     # got list of ISBNs
     # for each one
@@ -32,4 +49,37 @@ if __name__ == "__main__":
     #   else give a warning
 
     for each_isbn in args.isbn:
-        logging.debug(f"..handling {each_isbn}")
+        logging.debug(f"Handling ISBN: {each_isbn}")
+
+        url = "https://openlibrary.org/api/books"
+        params = {
+            "bibkeys": f"ISBN:{each_isbn}",
+            "format": "json",
+            "jscmd": "data"
+            }
+        headers = {
+            "User-Agent": "pyslims/0.0 (devangel77b@gmail.com)"
+            }
+        response = requests.get(url, params=params,headers=headers)
+        data = response.json()
+        book_key = f"ISBN:{each_isbn}"
+        if book_key in data:
+            book_info = data[book_key]
+            logging.debug(f"Found title: {book_info.get("title")}")
+
+            authors=", ".join([a['name'] for a in book_info.get("authors", [])])
+            pub_date = book_info.get("publish_date", "")
+            pub_year = pub_date[-4:] if len(pub_date) >= 4 else ""
+            publishers = ", ".join([p['name'] for p in book_info.get("publishers", [])])
+            writer.writerow({
+                "title": book_info.get("title", ""),
+                "isbn_issn": each_isbn,
+                "publisher_name": publishers,
+                "publish_year": pub_year,
+                "authors": authors
+            })
+            logging.debug(f"Successfully processed ISBN: {each_isbn}")
+        else:
+            logging.debug("Not found on Open Library, skipping.")
+
+    ofile.close()
